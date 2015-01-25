653d1a9afea1af42700130c6bc00474749a8eb05
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-24 20:40:56 -0800

    Basic core data preparation, creation, access and process.

--- Working With Core Data/ViewController.swift
+++ Working With Core Data/ViewController.swift
@@ -7,13 +7,50 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // +--- Database Preparation and Creation ---+
+        // +-----------------------------------------+
+        // Step1. Set up an App delegate to deal with core data
+        var appDel:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        // Step2. Set up a Framework in which core database can be accessed
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        // Step3. Construct database(each entity is just like a table)
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as NSManagedObject
+        newUser.setValue("Rob", forKey: "username")
+        newUser.setValue("pass", forKey: "password")
+        // Step4. Save the database
+        var newUserError:NSError? = nil
+        context.save(&newUserError)
+        
+        // +--- Database access ---+
+        // +-----------------------+
+        // Step1. Create the request to access the database
+        var request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        // Step2. Excute the request
+        var requestError:NSError? = nil
+        var results = context.executeFetchRequest(request, error: &requestError)
+        println(results)
+        // Step3. Process the result of the request
+        if results?.count > 0 {
+            for result:AnyObject in results! {
+                println(result.valueForKey("username"))
+                println(result.valueForKey("password"))
+            }
+        } else {
+            println("No database found")
+        }
+
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
